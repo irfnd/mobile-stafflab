7108b74d97bb1c8de729a766d184afc0b88c255b
irfnd <irfandiabimanyu@gmail.com>
2022-12-29 20:42:40 +0700

    [Add] - Setup project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+const moduleResolver = {
+	alias: {
+		assets: "./assets",
+		components: "./src/components",
+		constants: "./src/constants",
+		helpers: "./src/helpers",
+		navigations: "./src/navigations",
+		screens: "./src/screens",
+		states: "./src/states",
+		styles: "./src/styles",
+	},
+};
+
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [["module-resolver", moduleResolver], "transform-inline-environment-variables"],
+	};
 };
