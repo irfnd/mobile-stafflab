d3849be2a9863261d93b11a4237651d9f3f2565f
Irfnd <irfandiabimanyu@gmail.com>
2023-02-02 15:36:04 +0700

    [Update] - Seting build with eas

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 module.exports = {
 	name: "mobile-stafflab",
-	slug: "mobile-stafflab",
+	slug: "Stafflab Mobile",
 	version: "1.0.0",
 	owner: "irfnd",
 	orientation: "portrait",
@@ -13,7 +13,11 @@ module.exports = {
 	updates: { fallbackToCacheTimeout: 0 },
 	assetBundlePatterns: ["**/*"],
 	ios: { supportsTablet: true },
-	android: { adaptiveIcon: { foregroundImage: "./assets/adaptive-icon.png", backgroundColor: "#FFFFFF" } },
+	android: {
+		adaptiveIcon: { foregroundImage: "./assets/adaptive-icon.png", backgroundColor: "#FFFFFF" },
+		package: "com.stafflab.irfnd",
+		versionCode: 1,
+	},
 	web: { favicon: "./assets/favicon.png" },
 	extra: { eas: { projectId: "3b4d6063-eca5-467c-adef-2153401248a7" } },
 	packagerOpts: { config: "metro.config.js" },
