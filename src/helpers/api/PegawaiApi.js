80ab5988085995722ca23a4370cea9707ffd43b6
Irfnd <irfandiabimanyu@gmail.com>
2023-02-20 01:02:21 +0700

    [Update] - Laporan

--- src/helpers/api/PegawaiApi.js
+++ src/helpers/api/PegawaiApi.js
@@ -6,8 +6,23 @@ export const getPendidikan = async (nip) => await Supabase.from("pendidikan").se
 export const getMutasi = async (nip) => await Supabase.from("mutasi").select("*").eq("nipPegawai", nip);
 export const getCuti = async (nip) => await Supabase.from("cuti").select("*").eq("nipPegawai", nip);
 export const getDokumen = async (nip) => await Supabase.from("dokumen").select("*").eq("nipPegawai", nip);
-
 export const downloadFile = async (filePath) => await Supabase.storage.from("dokumen").download(filePath);
+
+// Upload & Post
+export const uploadFile = async (newData) => {
+	const { folder, kategori, namaFile, file, pegawai } = newData;
+	const filename = `${namaFile} - ${pegawai}.${file.name.match(/[^.]+$/)}`;
+	const { data, error } = await Supabase.storage.from("dokumen").upload(`${folder}/${kategori}/${filename}`, file);
+	if (error) throw error;
+	return { ...data };
+};
+
+export const postDokumen = async (newData) => {
+	const { nama, detail, nipPegawai, kategori } = newData;
+	const { data, error } = await Supabase.from("dokumen").insert({ nama, detail, nipPegawai, kategori }).select();
+	if (error) throw error;
+	return data[0];
+};
 
 export const postPengajuanCuti = async (data) => {
 	const { data: pengajuanCuti, error } = await Supabase.from("cuti").insert(data).select();
@@ -22,5 +37,7 @@ export default {
 	getMutasi,
 	getCuti,
 	getDokumen,
+	uploadFile,
+	postDokumen,
 	postPengajuanCuti,
 };
--- src/helpers/hooks/useAuth.js
+++ src/helpers/hooks/useAuth.js
@@ -3,14 +3,26 @@ import { useEffect } from "react";
 import { store } from "states/store";
 import { AuthActions } from "states/slices/AuthSlice";
 
+const checkClaims = (session) => {
+	if (session) {
+		const claims = session.user.app_metadata?.claims;
+		if (claims) {
+			if (claims !== "ADMIN") {
+				store.dispatch(AuthActions.setSession(session));
+			} else {
+				store.dispatch(AuthActions.setSession(null));
+			}
+		} else {
+			store.dispatch(AuthActions.setSession(session));
+		}
+	} else {
+		store.dispatch(AuthActions.setSession(null));
+	}
+};
+
 export default function useAuth() {
 	useEffect(() => {
-		Supabase.auth.getSession().then(({ data: { session } }) => {
-			store.dispatch(AuthActions.setSession(session));
-		});
-
-		Supabase.auth.onAuthStateChange((_event, session) => {
-			store.dispatch(AuthActions.setSession(session));
-		});
+		Supabase.auth.getSession().then(({ data: { session } }) => checkClaims(session));
+		Supabase.auth.onAuthStateChange((_event, session) => checkClaims(session));
 	}, []);
 }
